0991d54d8ab8b2e4d805f9ad83263ad2d0912d19
Sooraj Lapasi <soorajlapasi@gmail.com>
2026-04-09 00:34:54 +0530

    updated ci-cd yaml files

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # aws-ci
+
+ImagePullBackOff → wrong image name
+CrashLoopBackOff → app issue
+Service not reachable → port mismatch
+
+kubectl rollout restart deployment api-service -n devops
--- api-service/main.py
+++ api-service/main.py
@@ -2,15 +2,29 @@ from fastapi import FastAPI
 import redis
 import json
 
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
+from fastapi.responses import Response
+
 app = FastAPI()
 
+# Prometheus Counter
+REQUEST_COUNT = Counter('request_count', 'Total API Requests')
+
+# Redis connection
 r = redis.Redis(host="redis", port=6379, decode_responses=True)
 
 @app.get("/")
 def read_root():
+    REQUEST_COUNT.inc()
     return {"message": "API Service Running"}
 
 @app.post("/task")
 def add_task(data: dict):
+    REQUEST_COUNT.inc()
     r.lpush("tasks", json.dumps(data))
     return {"status": "task added"}
+
+# ✅ Metrics endpoint (CRITICAL FIX)
+@app.get("/metrics")
+def metrics():
+    return Response(generate_latest(), media_type=CONTENT_TYPE_LATEST)
